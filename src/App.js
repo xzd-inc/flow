b15ed1ac012ef8fe31c252b2e0353c5c8f4d09a1
xzd-inc <810792608@Outlook.com>
2023-04-18 00:11:24 +0800

    feat: 初始化

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import data from './data.json'
+
+import WorkFlow from './components/WorkFlow'
+
+const config = data.data.nodeConfig
+
+console.log(config)
+
+function App() {
+  return (
+    <div className="App">
+      <WorkFlow config={config} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nodes/AddOption.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AddNodeOption(props) {
+  return (
+    <a
+      className={"add-node-popover-item " + props.type}
+      onClick={props.onClick}
+    >
+      <div className="item-wrapper">
+        <span className="iconfont"></span>
+      </div>
+      <p>{props.name}</p>
+    </a>
+  );
+}
+
+export default AddNodeOption;
